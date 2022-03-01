87035f786f37ac1d26de50e03e5147d5e440a343
vr3n <vickyspatel@gmail.com>
2022-03-01 22:59:09 +0530

    Initial COmmit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:real_estate_ui/utils/constants.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = window.physicalSize.width;
+    return MaterialApp(
+      title: 'Real Estate UI Build',
+      theme: ThemeData(
+        primaryColor: COLOR_WHITE,
+        textTheme: screenWidth < 500 ? TEXT_THEME_SMALL : TEXT_THEME_DEFAULT,
+        fontFamily: 'Montserrat',
+      ),
+      home: const Text("Home Page"),
+    );
+  }
+}
